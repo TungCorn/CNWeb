b1bc1a1dc30033f093bb30d4ec7d53193eea8f37
TungCorn <tung01012005s@gmail.com>
2025-12-11 19:26:47 +0700

    feat: Add initial application files including configuration, routing, and basic models

---
+++ BT_LOP/lr-project/database/seeders/PostSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+        for ($i = 0; $i < 100; $i++) {
+            Post::create([
+                'title' => $faker->sentence,
+                'content' => $faker->paragraph
+            ]);
+        }
+    }
+}
